62881e7c037d4a40b1980d2bc14020a4156fca7c
HannaBM12 <Hannabmulugeta@gmail.com>
2021-02-07 00:44:36 -0500

    Done.

--- db/migrate/20160113163910_add_category_id_to_posts.rb
+++ db/migrate/20160113163910_add_category_id_to_posts.rb
@@ -1,5 +1,5 @@
 class AddCategoryIdToPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :category_id, :integer
+    add_column :posts, :category_id, :integer, post_status:string
   end
 end
